4c45b69261187dfbb655d73abacfd381d90e5373
Vale Rojas <valerojasu98@hotmail.com>
2024-06-06 15:11:49 -0600

    responsivees

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -8,18 +8,14 @@ function Footer(){
         <div className="footerm">
             <Container >
       <Row>
-        <Col><h4 className="titulosf">Copyright &#64;2023 - Created by Valeria Rojas</h4>
+        <Col sm={6}>
+        <h4 className="titulosf">Copyright @ 2023 - Created by Valeria Rojas</h4>
         </Col>
 
-        <Col>
-        <h4 className="titulosf">Terms & Condition</h4> 
-        </Col>
-        <Col>
-        <h4 className="titulosf">Privacy Policy</h4>
-        </Col>
-        <Col>
-        <h4 className="titulosf">Contáctenos</h4> 
-            <a href="mailto:sudisplay@gmail.com" className="subf" >sudisplay&#64;gmail.com</a>        </Col>
+        <Col sm={6}>
+        <h4 className="titulosf">Contáctenos al:  
+            <a href="mailto:sudisplay@gmail.com" className="subf" > sudisplay&#64;gmail.com</a></h4>       
+            </Col>
       </Row>
       
     </Container>
