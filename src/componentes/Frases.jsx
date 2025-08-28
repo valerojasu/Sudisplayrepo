950b58a648d6998064d6cca2468d633aa6758dcb
Vale Rojas <valerojasu98@hotmail.com>
2025-08-28 10:22:18 -0600

    foto changes

--- src/componentes/Frases.jsx
+++ src/componentes/Frases.jsx
@@ -1,57 +1,58 @@
 import React from "react";
-import { Container, Row, Col, Carousel } from "react-bootstrap"; // Usamos Carousel de react-bootstrap
-import '../css/Frases.css';
-import Disp1 from '../Imagenes/Display/Dis1.png';
-import Disp2 from '../Imagenes/Display/Dis2.png';
-import Disp3 from '../Imagenes/Display/Dis3.png';
-import Disp4 from '../Imagenes/Display/Dis4.png';
-import Disp5 from '../Imagenes/Display/Dis5.png';
-import Disp6 from '../Imagenes/Display/Dis6.png';
-import Disp7 from '../Imagenes/Display/Dis7.png';
-import Disp8 from '../Imagenes/Display/Dis8.png';
-import Disp9 from '../Imagenes/Display/Dis9.png';
+import { Container, Row, Col, Carousel } from "react-bootstrap";
+import "../css/Frases.css";
+
+import Disp1 from "../Imagenes/imag-dis/Disp1.png";
+import Disp2 from "../Imagenes/imag-dis/Disp2.png";
+import Disp3 from "../Imagenes/imag-dis/Disp3.png";
+import Disp4 from "../Imagenes/imag-dis/Disp4.png";
+import Disp5 from "../Imagenes/imag-dis/Disp5.png";
+import Disp6 from "../Imagenes/imag-dis/Disp6.png";
+import Disp7 from "../Imagenes/imag-dis/Disp7.png";
+import Disp8 from "../Imagenes/imag-dis/Disp8.png";
+import Disp9 from "../Imagenes/imag-dis/Disp9.png";
+import Disp10 from "../Imagenes/imag-dis/Disp10.png";
+
 function Frases(props) {
   return (
     <div className="frase">
       <Container>
         <Row className="frase-row">
-          {/* Columna para el texto */}
+          {/* Columna Texto */}
           <Col className="col-text">
             <h1 className="titulo2">{props.pregunta}</h1>
             <h2 className="desc">{props.descripcion}</h2>
           </Col>
 
-          {/* Columna para el Slider usando Carousel */}
+          {/* Columna Carrusel */}
           <Col className="col-slider">
-            <Carousel interval={3000} controls={false} indicators={true} fade>
-              <Carousel.Item>
-                <img src={Disp1} alt="Imagen 1" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp2} alt="Imagen 2" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp3} alt="Imagen 3" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp4} alt="Imagen 4" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp5} alt="Imagen 5" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp6} alt="Imagen 6" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp7} alt="Imagen 7" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp8} alt="Imagen 8" className="slide-image" />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img src={Disp9} alt="Imagen 9" className="slide-image" />
-              </Carousel.Item>
-            </Carousel>
+            <div className="carousel-card">
+              <Carousel
+                interval={3500}
+                controls={false}
+                indicators={true}
+                fade
+                pause={false}
+                wrap={true}
+              >
+                {[
+                  Disp1,
+                  Disp2,
+                  Disp3,
+                  Disp4,
+                  Disp5,
+                  Disp6,
+                  Disp7,
+                  Disp8,
+                  Disp9,
+                  Disp10,
+                ].map((img, i) => (
+                  <Carousel.Item key={i}>
+                    <img src={img} alt={`Imagen ${i + 1}`} />
+                  </Carousel.Item>
+                ))}
+              </Carousel>
+            </div>
           </Col>
         </Row>
       </Container>
--- src/componentes/Indicadores.jsx
+++ src/componentes/Indicadores.jsx
@@ -1,38 +1,43 @@
 import React from "react";
-import '../css/Indicadores.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import "../css/Indicadores.css";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Indicadores() {
   const indicadores = [
     { id: 1, title: "Mercaderistas 2.0" },
     { id: 2, title: "APP" },
     { id: 3, title: "Impulsadoras y modelos" },
-    { id: 4, title: "Mediciones de la ejecución según lo negociado en el punto de venta" },
+    {
+      id: 4,
+      title:
+        "Mediciones de la ejecución según lo negociado en el punto de venta",
+    },
     { id: 5, title: "Censos de precio de competencia" },
     { id: 6, title: "Estandarización de procesos" },
     { id: 7, title: "Activación BTL del PDV" },
     { id: 8, title: "Comandos de ejecución" },
+    { id: 9, title: "End to end" },
   ];
 
   return (
     <div className="infor">
       <Container>
         <Row>
-          {indicadores.slice(0, 3).map(indicador => (
+          {indicadores.slice(0, 3).map((indicador) => (
             <Col key={indicador.id} className="indi" xs={12} md={4}>
               <h3 className="indif">{indicador.title}</h3>
             </Col>
           ))}
         </Row>
         <Row>
-          {indicadores.slice(3, 6).map(indicador => (
+          {indicadores.slice(3, 6).map((indicador) => (
             <Col key={indicador.id} className="indi" xs={12} md={4}>
               <h3 className="indif">{indicador.title}</h3>
             </Col>
           ))}
         </Row>
         <Row>
-          {indicadores.slice(6).map(indicador => (
+          {indicadores.slice(6, 9).map((indicador) => (
             <Col key={indicador.id} className="indi" xs={12} md={6}>
               <h3 className="indif">{indicador.title}</h3>
             </Col>
